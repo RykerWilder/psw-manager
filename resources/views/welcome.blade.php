10264097fe016428e35a6d7933f73267fc8a82af
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-01-23 10:21:51 +0100

    add: fontawesome tag in app Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
         <h1 class="text-center mb-5">Welcome to iLock</h1>
 
         <div class="mt-5 d-flex justify-content-around">
-            <div class="p-5 border border-black">Unlimited password and devices</div>
-            <div class="p-5 border border-black">Easy to use</div>
+            <div class="p-5 border border-black"><h5>Unlimited password and devices</h5></div>
+            <div class="p-5 border border-black"><h5>Easy to use</h5></div>
             <div class="p-5 border border-black"><i class="fa-solid fa-gear"></i><h5>Generate and manage your password</h5></div>
         </div>
     </div>
