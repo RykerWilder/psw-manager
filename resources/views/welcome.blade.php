8732a86f15a0b9ec6bab9be56344fa53b9fbca01
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-01-23 10:14:46 +0100

    add: cards in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <div class="p-5">
-        <h1 class="text-center">Welcome to iLock</h1>
+        <h1 class="text-center mb-5">Welcome to iLock</h1>
+
+        <div class="mt-5 d-flex justify-content-around">
+            <div class="p-5 border border-black">Unlimited password and devices</div>
+            <div class="p-5 border border-black">Easy to use</div>
+            <div class="p-5 border border-black"><i class="fa-solid fa-gear"></i><h5>Generate and manage your password</h5></div>
+        </div>
     </div>
 @endsection
