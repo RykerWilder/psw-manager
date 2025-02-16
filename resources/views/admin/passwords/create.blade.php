9c7cf7a190a0b0dfef464783196cbf75db037277
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-02-16 10:33:58 +0100

    style: admin/password/create.scss

--- resources/views/admin/passwords/create.blade.php
+++ resources/views/admin/passwords/create.blade.php
@@ -1,56 +1,54 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
+<div class="form-container">
+    <h1>Create a new password</h1>
+    <form action="{{ route('admin.passwords.store') }}" method="POST">
+        @csrf
 
-            
-
-            <form action="{{ route('admin.passwords.store') }}" method="POST">
-                @csrf
-
-                {{-- name --}}
-                <div class="form-floating mb-3 mt-5">
-                    <input type="text" class="form-control" id="floatingInput" name="name">
-                    <label for="floatingInput">Name*</label>
-                </div>
-
-                {{-- username --}}
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" name="username">
-                    <label for="floatingInput">Username*</label>
-                </div>
+        <section>
+            {{-- name --}}
+            <div class="form-input ">
+                <input type="text" class="border" id="floatingInput" name="name">
+                <label for="floatingInput">Name*</label>
+            </div>
     
-                {{-- password length --}}
-                {{-- <div class="form-label mb-3 mt-3">
-                    <label for="customRange2">Password length</label>
-                    <input type="range" class="form-range" min="0" max="5" id="customRange2">
-                </div> --}}
+            {{-- username --}}
+            <div class="form-input">
+                <input type="text" class="border" id="floatingInput" name="username">
+                <label for="floatingInput">Username*</label>
+            </div>
     
-                {{-- password --}}
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="floatingPassword" name="password">
-                    <label for="floatingPassword">Password*</label>
-                </div>
+            {{-- password --}}
+            <div class="form-input">
+                <input type="password" class="border" id="floatingPassword" name="password">
+                <label for="floatingPassword">Password*</label>
+            </div>
+        </section>
+        <section>
+            {{-- password length --}}
+            {{-- <div class="form-label mb-3 mt-3">
+                        <label for="customRange2">Password length</label>
+                        <input type="range" class="form-range" min="0" max="5" id="customRange2">
+                    </div> --}}
     
-                {{-- favourite --}}
-                <div class="mb-3">
-                    <input class="form-check-input" type="checkbox" value="1" id="flexCheckDefault" name="favourite">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        Add to favourites
-                    </label>
-                </div>
+            {{-- favourite --}}
+            <div class="">
+                <label class="" for="flexCheckDefault">
+                    Add to favourites
+                </label>
+                <input checked="" class="check" type="checkbox" value="1" id="flexCheckDefault" name="favourite">
+            </div>
     
-                {{-- color picker --}}
-                <div class="mb-3">
-                    <label for="exampleColorInput" class="form-label">Choose a color</label>
-                    <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c"
-                        title="Choose your color" name="color">
-                </div>
+            {{-- color picker --}}
+            <div class="color-picker">
+                <label for="exampleColorInput">Choose a color</label>
+                <input type="color" id="exampleColorInput" value="#78be78"
+                    title="Choose your color" name="color">
+            </div>
+            <button type="submit" class="border">Create</button>
+        </section>
 
-                <button type="submit" class="btn border border-dark mt-4 ms-button">Create</button>
-            </form>
-
-        </div>
-    </div>
+    </form>
+</div>
 @endsection
