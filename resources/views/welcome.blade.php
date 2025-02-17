8d0c00df70c2faf8d5e3913d55276b282834408b
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-02-17 11:19:22 +0100

    style: width card in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
     <div class="container-welcome">
         <h1>Welcome to iLock</h1>
         <ul>
-            <li class="border"><h5>Unlimited password and devices</h5></li>
-            <li class="border"><h5>Easy to use</h5></li>
-            <li class="border"><h5>Generate and manage your passwords</h5></li>
+            <li class="border"><i class="fa-solid fa-infinity"></i><h5>Unlimited password and devices</h5></li>
+            <li class="border"><i class="fa-solid fa-gear"></i><h5>Easy to use</h5></li>
+            <li class="border"><i class="fa-solid fa-sliders"></i><h5>Generate and manage your passwords</h5></li>
         </ul>
     </div>
 @endsection
